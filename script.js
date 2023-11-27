65ceb178e93993f8fdfc13327d0b2a306656ca05
vengadesan2 <vengadsanguna24@gmail.com>
2023-11-27 12:35:17 +0530

    call hell

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  document.getElementById("h1").innerText = "10";
+  setTimeout(() => {
+    document.getElementById("h1").innerText = "9";
+    setTimeout(() => {
+      document.getElementById("h1").innerText = "8";
+      setTimeout(() => {
+        document.getElementById("h1").innerText = "7";
+        setTimeout(() => {
+          document.getElementById("h1").innerText = "6";
+          setTimeout(() => {
+            document.getElementById("h1").innerText = "5";
+            setTimeout(() => {
+              document.getElementById("h1").innerText = "4";
+              setTimeout(() => {
+                document.getElementById("h1").innerText = "3";
+                setTimeout(() => {
+                  document.getElementById("h1").innerText = "2";
+                  setTimeout(() => {
+                    document.getElementById("h1").innerText = "1";
+                    setTimeout(() => {
+                      document.getElementById("h1").innerText =
+                        "happy independence day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
